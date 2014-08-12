deef742616d8f0c3a623e628a338b58f59e0ae37
Ed Ropple <ed@edropple.com>
2014-08-12 10:44:12 -0400

    stubbed out S3 and SNS outputs

---
+++ extensions/ec2-worker/src/main/scala/com/edcanhack/spinal/extensions/ec2/worker/effectors/outputs/SNS.scala
@@ -0,0 +1,9 @@
+package com.edcanhack.spinal.extensions.ec2.worker.effectors.outputs
+
+import com.edcanhack.spinal.worker.effectors.TemplateEffector.Output
+
+case class SNS() extends Output {
+  override def write(str: String): Unit = ???
+
+  ???
+}
